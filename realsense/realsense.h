a11fbd0fcd5c93c1ccb2e221dcabf85d77453f00
zhongsy <1278768717@qq.com>
2020-08-25 13:52:38 +0800

    开启多线程失败

--- realsense/realsense.h
+++ realsense/realsense.h
@@ -3,6 +3,7 @@
 
 #include<librealsense2/rs.hpp>
 #include<opencv2/opencv.hpp>
+#include<iostream>
 
 #define width 848
 #define height 480 
@@ -20,56 +21,63 @@ private:
     rs2::hole_filling_filter hole_filter;
     rs2::colorizer colorizered; 
     rs2::device_list dev_list;  
-    rs2::device dev; 
+    rs2::device dev;
+    cv::Mat depth_data; 
 
 public:
     realsense();
     ~realsense();
     void init();
-    cv::Mat get_depth();
+    void get_depth();
+    void imshow_colorized_depth();
+    void get_data(void *data);
    
 };
 
-realsense::realsense()
+void realsense::get_data(void *data)
 {
-    rs2::spatial_filter spat_filter(0.4f,4.0f,2.0f,0);
-    rs2::threshold_filter thd_filter(0.15f,4.0f);
-    rs2::hole_filling_filter hole_filter(0);
-    rs2::colorizer colorizered(0);
+    auto cdata=static_cast<cv::Mat *>(data);
+    *cdata=depth_data;
+}
+
+
+realsense::realsense():spat_filter(0.4f,4.0f,2.0f,0),thd_filter(0.15f,4.0f),hole_filter(0),colorizered(0)
+{
+   
 }
 
 realsense::~realsense()
 {
 }
 
-cv::Mat realsense::get_depth()
+void realsense::get_depth()
 {
     frames=pipe.wait_for_frames();
     rs2::depth_frame depth_frame=frames.get_depth_frame();
     cv::Mat depth(cv::Size(width,height),CV_16UC1,(void*)depth_frame.get_data(),cv::Mat::AUTO_STEP);
     cv::imshow("depth",depth);
-    cv::waitKey(1);    
+    cv::waitKey(1);
+    depth.copyTo(depth_data);    
 
 }
 
 void realsense::init()
 {
 
-    rs2::context ctx;
+    //rs2::context ctx;
     //获取设备列表
     dev_list=ctx.query_devices();
     if(dev_list.size()==0)
     throw std::runtime_error("No device detected.");
 
     dev=dev_list.front();
-    rs2::frameset frames;
-    rs2::pipeline pipe(ctx);
-
+    rs2::pipeline pipe1(ctx);
+    pipe=pipe1;
     //rs2::decimation_filter dec_filter;
-    rs2::spatial_filter spat_filter(0.4f,4.0f,2.0f,0);
-    rs2::threshold_filter thd_filter(0.15f,4.0f);
-    rs2::hole_filling_filter hole_filter(0);
-    rs2::colorizer colorizered;
+    // rs2::spatial_filter spat_filter(0.4f,4.0f,2.0f,0);
+    // rs2::threshold_filter thd_filter(0.15f,4.0f);
+    // rs2::hole_filling_filter hole_filter(0);
+    // rs2::colorizer colorizered;
 
     //dec_filter.set_option(RS2_OPTION_FILTER_MAGNITUDE,3);
     // spat_filter.set_option(RS2_OPTION_FILTER_SMOOTH_ALPHA,0.4f);
@@ -81,10 +89,19 @@ void realsense::init()
     cfg.enable_stream(RS2_STREAM_INFRARED, 1, width, height, RS2_FORMAT_Y8, fps);
 	cfg.enable_stream(RS2_STREAM_INFRARED, 2, width, height, RS2_FORMAT_Y8, fps);
     pipe.start(cfg);
+    std::cout<<"device start"<<std::endl;
 }
 
-
-
+void realsense::imshow_colorized_depth()
+{
+    frames=pipe.wait_for_frames();
+    rs2::frame coloried=frames.get_depth_frame().apply_filter(colorizered);
+    cv::Mat color(cv::Size(width,height),CV_8UC3,(void*)coloried.get_data(),cv::Mat::AUTO_STEP);
+    cv::imshow("coloried",color);
+    cv::waitKey(1);
+    
+    
+}
 
 
 
--- realsense/thread.cc
+++ realsense/thread.cc
@@ -1,15 +1,21 @@
 #include<librealsense2/rs.hpp>
 #include<opencv2/opencv.hpp>
 #include"realsense.h"
-
+#include<thread>
+#include<functional>
 
 int main()
 {
     realsense dev_1;
     dev_1.init();
+    cv::Mat filtered;
+    std::shared_ptr<std::thread> run_executor_= std::make_shared<std::thread>(
+      std::bind(&realsense::get_depth, dev_1));
+
     while(1)
     {
-        dev_1.get_depth();
+        // dev_1.imshow_colorized_depth();
+        // cv::waitKey(100);
     }
     
 }
